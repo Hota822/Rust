7c7d660add715a826be60be48e9d237ffd4e54c5
hota822 <hota822@gmail.com>
2020-01-17 09:36:15 +0000

    made HTTP request API put on qiita

--- rocket-webapi/target/debug/rocket-webapi.d
+++ rocket-webapi/target/debug/rocket-webapi.d
@@ -1,1 +1,1 @@
-/opt/rustup/rocket-webapi/target/debug/rocket-webapi: /opt/rustup/rocket-webapi/src/main.rs
+/opt/rustup/rocket-webapi/target/debug/rocket-webapi: /opt/rustup/rocket-webapi/src/main.rs /opt/rustup/rocket-webapi/src/models.rs /opt/rustup/rocket-webapi/src/routes.rs
